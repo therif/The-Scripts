1fcdb872bc529116166a3c2d9e47e7f2e804cee5
therif <therif@users.noreply.github.com>
2024-03-06 18:00:28 +0700

    Create amlogin_afterinstall.sh

---
+++ Armbian/amlogin_afterinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# My Hello, World! script
+echo "Hello, World! - from The-Scripts"
+
+# Disable Armbian Log
+systemctl stop armbian-ramlog.service
+systemctl disable armbian-ramlog.service
+
+#Update Pakcage
+sudo apt update -y
+
+#Upgrade and replace
+sudo apt upgrade -y
+
+#Then run manual NMTUI to set network
+echo "Run on terminal with command: nmtui"
